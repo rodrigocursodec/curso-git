cf8d98f4483782aea7035ef7d4cb5460bbb7ce24
Rodrigo Granja <rodrigohg18@hotmail.com>
2020-10-20 12:44:00 -0300

    Teste

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,15 +9,11 @@ namespace ConsoleApp1
         public static void Main(string[] args)
         {
 
-            TimeSpan t1 = new TimeSpan(); //00:00:00
-            TimeSpan t2 = new TimeSpan(900000000); //00:01:30
-            TimeSpan t3 = new TimeSpan(2, 11, 10); //02:11:10
-            TimeSpan t4 = new TimeSpan(5, 2, 11, 10); //5.02:11:10
+            
 
 
 
-            Console.WriteLine(t4);
-            Console.WriteLine(t3);
+            Console.WriteLine("Teste");
 
         }
 
--- ConsoleApp1/RacunhoDateTime.cs
+++ ConsoleApp1/RacunhoDateTime.cs
@@ -25,5 +25,16 @@ namespace ConsoleApp1
         //Console.WriteLine(d1.Ticks<d2.Ticks? "d1" : "d2");
 
         //Ticks são 100 nanosegundos
+
+
+
+        //TimeSpan t1 = new TimeSpan(); //00:00:00
+        //TimeSpan t2 = new TimeSpan(900000000); //00:01:30
+        //TimeSpan t3 = new TimeSpan(2, 11, 10); //02:11:10
+        //TimeSpan t4 = new TimeSpan(5, 2, 11, 10); //5.02:11:10
+        //TimeSpan t5 = new TimeSpan(5, 2, 11, 10, 320); //5.02:11:10.3200000
+
+        //TimeSpan t10 = TimeSpan.FromDays(1.5); //1.12:00:00
+        //TimeSpan t11 = TimeSpan.FromHours(1.5); //01:30:00
     }
 }
